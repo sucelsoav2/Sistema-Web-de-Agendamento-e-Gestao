d812cb5c5a5425d818e82e03fea1d87bc2f5a66d
LeoMagal <lmagal2003@gmail.com>
2026-05-13 10:09:07 -0300

    adicao do nodemon adicao da primeira rota para teste formatacao com prettier do front

---
+++ BACKEND/src/routes/authRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/home", (req, res) => {
+  res.status(200).json({ mensagem: "Home da autenticação" });
+});
+module.exports = router;
